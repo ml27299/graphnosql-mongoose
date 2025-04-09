689e9e80db3cb28a780e0de3d777fe587997d445
Macario Lara <ml27299@gmail.com>
2025-04-08 23:35:48 -0500

    update

--- Schema/libs/SchemaPermissionHelper.js
+++ Schema/libs/SchemaPermissionHelper.js
@@ -20,7 +20,7 @@ class SchemaPermissionHelper {
 		return shield(
 			this.permissions.reduce(
 				(result, permission) => Object.assign(result, permission),
-				{ allowExternalErrors: true }
+				{}
 			)
 		);
 	}
